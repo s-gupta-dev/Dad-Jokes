584e72aae375626141e4819ed906c171a2256f93
Sucheta Gupta <sucheta.gupta.dev@gmail.com>
2025-02-21 16:06:18 -0500

    set up fetch request from API to display Dad Jokes

---
+++ script.js
@@ -0,0 +1,16 @@
+const jokeEl = document.getElementById("joke");
+const jokeBtn = document.getElementById("jokeBtn");
+
+async function generateJoke() {
+  const config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+
+  const res = await fetch("https://icanhazdadjoke.com", config);
+  const data = await res.json();
+  jokeEl.textContent = data.joke;
+}
+
+generateJoke();
